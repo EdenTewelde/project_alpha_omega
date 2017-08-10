5666602c9e7b4a52f8c18061f5a1ba75057cf911
Eden Tewelde <eden.tewelde@th-bingen.de>
2017-08-10 08:02:12 +0200

    add method to randomize decision if car is delayed

--- src/main/kotlin/Network.kt
+++ src/main/kotlin/Network.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 class Network(val capacity: Int, val listOfCars: MutableList<Car>) {
 
     val listOfCarsDriving: MutableList<Car> = mutableListOf()
@@ -25,4 +27,47 @@ class Network(val capacity: Int, val listOfCars: MutableList<Car>) {
         }
     }
 
+    fun randomize() : Boolean {
+        var isDelayed = false
+        val randomNumber = Random()
+        val random = randomNumber.nextInt(101)
+        val utilization = (this.listOfCarsDriving.size/this.capacity)
+        if (utilization < 50) {
+            when (random) {
+                in 0..5 -> isDelayed = true
+                in 6..100 -> isDelayed = false
+            }
+        } else if (utilization < 70) {
+            when (random) {
+                in 0..10 -> isDelayed = true
+                in 11..100 -> isDelayed = false
+            }
+        } else if (utilization < 90) {
+            when (random) {
+                in 0..20 -> isDelayed = true
+                in 21..100 -> isDelayed = false
+            }
+        } else if (utilization < 110) {
+            when (random) {
+                in 0..30 -> isDelayed = true
+                in 31..100 -> isDelayed = false
+            }
+        } else if (utilization < 125) {
+            when (random) {
+                in 0..50 -> isDelayed = true
+                in 51..100 -> isDelayed = false
+            }
+        } else if (utilization < 150) {
+            when (random) {
+                in 0..70 -> isDelayed = true
+                in 71..100 -> isDelayed = false
+            }
+        } else if (utilization > 150) {
+            when (random) {
+                in 0..90 -> isDelayed = true
+                in 91..100 -> isDelayed = false
+            }
+        }
+        return isDelayed
+    }
 }
